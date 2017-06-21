6028e0be6bd66179d93b1a70ad14a25e4f453bec
ymamane5 <ymamane5@gmail.com>
2017-06-21 20:51:05 +0300

    class

--- CODE/MATCommon/src/entities/CClass.java
+++ CODE/MATCommon/src/entities/CClass.java
@@ -8,18 +8,25 @@ import java.util.ArrayList;
 
 public class CClass implements Serializable{
 	public String classNumber;
+	public String className;
     public ArrayList<Student> studentList;
     
-    public CClass(String number) {
+    public CClass(String number, String name) {
     	classNumber = number;
-    }
-	  
+    	className = name;
+    }  
 	  
 	public String getClassNumber() {
 		return classNumber;
 	}
 	public void setClassNumber(String classNumber) {
 		this.classNumber = classNumber;
+	}
+	public String getClassNane() {
+		return className;
+	}
+	public void setClassName(String className) {
+		this.className = className;
 	}
 	public Boolean addStudent(Student newStudent) {
 		return studentList.add(newStudent);
