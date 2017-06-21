f308bce1205d7e69cec15ba52d51b6ed45cf93b4
ymamane5 <ymamane5@gmail.com>
2017-06-21 15:54:05 +0300

    define class

---
+++ CODE/MATClient/src/MATGui/AddTeacherToCourseClassController.java
@@ -0,0 +1,43 @@
+package MATGui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+
+public class AddTeacherToCourseClassController {
+
+    @FXML
+    private TextField classId;
+
+    @FXML
+    private TextField teacherId;
+
+    @FXML
+    private Button btnAdd;
+
+    @FXML
+    private TextField courseId;
+
+    @FXML
+    void AddTeacher(ActionEvent event) {
+    	
+    	String tid = teacherId.getText().toString();
+    	String coid = courseId.getText().toString();
+    	String clid = classId.getText().toString();
+    	/* send request to server
+    	*  add teacher
+    	*/
+    	
+    	// if succeeded
+    	Alert alert = new Alert(AlertType.INFORMATION);
+    	alert.setHeaderText("teacher " + tid + " added successfully to course " + coid);
+    	alert.show();
+    }
+
+}
+
+
+
