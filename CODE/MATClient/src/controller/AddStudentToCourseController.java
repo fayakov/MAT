08f0997dad6d9d2a481f89dfda568a75c98f15a0
sapirSegall <sap456@walla.com>
2017-06-22 21:37:29 +0300

    add student to course

---
+++ CODE/MATClient/src/controller/AddStudentToCourseController.java
@@ -0,0 +1,35 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class AddStudentToCourseController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField studentIdTextField;
+
+    @FXML
+    private TextField courseTextField;
+
+    @FXML
+    private TextField classTextField;
+
+    @FXML
+    void sendRequest(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+}
