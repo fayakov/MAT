23db9311aa69c442fa714847cf9edbe6d596cc54
ymamane5 <ymamane5@gmail.com>
2017-06-23 20:58:20 +0300

    student menu

--- CODE/MATClient/src/controller/StudentController.java
+++ CODE/MATClient/src/controller/StudentController.java
@@ -1,8 +1,6 @@
 package controller;
 
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -11,38 +9,24 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class StudentController {
 	
 	ObservableList<String> list ;
+	String option;
 
     @FXML
-    private ResourceBundle resources;
-
-    @FXML
-    private URL location;
-
-    @FXML
-    private Label labelStudentMenu;
-
-    @FXML
-    private ComboBox<String> comboStudentMenu;
-
-    @FXML
-    private Button btnSend;
-
-    @FXML
-    public void StudentInfo(ActionEvent event) {
-    	
-		String option = comboStudentMenu.getValue().toString();
-    	/*if(option.equalsIgnoreCase("Student Data"))
-    		studentData1();	
-    	if(option.equalsIgnoreCase("Assignment"))
-    		assignment();*/	
+    private ComboBox<String> optionCombo;
+    
+    public void studentData1() throws Exception {
+    	Pane root = FXMLLoader.load(getClass().getResource("/gui/StudentData.fxml"));
+		Scene scene = new Scene(root);
+		Stage primaryStage = new Stage();
+		primaryStage.setScene(scene);
+		primaryStage.show();
     }
     
     public void start(Stage primaryStage) throws Exception {
@@ -51,10 +35,17 @@ public class StudentController {
 		Scene scene = new Scene(root);
 		primaryStage.setScene(scene);
 		primaryStage.show();
-	}
+	}   
     
-    
-    
+    @FXML
+    public void StudentInfo(ActionEvent event) throws Exception {
+    	
+	option = optionCombo.getValue().toString();
+    	if(option.equalsIgnoreCase("Student Data"))
+    		studentData1();	
+    	//if(option.equalsIgnoreCase("Assignment"))
+    		//assignment();*
+    }
 
     @FXML
     void initialize() {
@@ -62,10 +53,9 @@ public class StudentController {
     	
     	options.add("Student Data");
       	options.add("Assignment"); 
-		
-		
+			
 		list = FXCollections.observableArrayList(options);
-		comboStudentMenu.setItems(list);
+		optionCombo.setItems(list);
 
     }
 }
--- CODE/MATClient/src/test/studentGuiTest.java
+++ CODE/MATClient/src/test/studentGuiTest.java
@@ -1,17 +1,8 @@
 package test;
 
-
-import java.net.URL;
 import controller.StudentController;
-import entities.Student;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-
 
 //TAL
 public class studentGuiTest extends Application 
