785e99ba7abba4ff3d86e961ffb97fc634fc1b74
sapirSegall <sap456@walla.com>
2017-06-24 15:51:24 +0300

    בקשה להוספת סטודנט לקורס

--- CODE/MATClient/src/controller/AddStudentToCourseController.java
+++ CODE/MATClient/src/controller/AddStudentToCourseController.java
@@ -6,14 +6,16 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class AddStudentToCourseController {
 	
 	int sid, cid, oid;
+	
+	String isConfirmed, isHandeled;
 
     @FXML
     private ResourceBundle resources;
@@ -40,19 +42,24 @@ public class AddStudentToCourseController {
     		Alert alert = new Alert(AlertType.ERROR);
     		alert.setHeaderText("one or more of the fields is empty");
     		alert.show();
+    		Prompt.alert(3,"one or more of the fields is empty");
     	}
-    	else { // if succeeded
-    		
-	    	/* add student to course */
-    		sid = Integer.parseInt(studentIdTextField.getText());
-        	cid = Integer.parseInt(classTextField.getText());
-        	oid = Integer.parseInt(courseTextField.getText());
-    		Alert alert = new Alert(AlertType.INFORMATION);
-    		alert.setHeaderText("student " + sid + " added successfully to class " + cid);
-    		alert.show();
+    	else { // add request to db
+    		try {
+    			sid = Integer.parseInt(studentIdTextField.getText());
+    			cid = Integer.parseInt(classTextField.getText());
+    	    	oid = Integer.parseInt(courseTextField.getText());
+    	    	isConfirmed = null;
+    	    	isHandeled = null;
+    	    	
+    	    	} catch(NumberFormatException e){
+    	    	Prompt.alert(3,"please enter numerical value");
+    	    	return;
+    	    	}  		
+        	Prompt.alert(2,"The request added successfully ");
     	}
     }
-
+    
     @FXML
     void closeRequest1(ActionEvent event) {
         Stage stage = (Stage) btnClose.getScene().getWindow();
@@ -60,8 +67,4 @@ public class AddStudentToCourseController {
 
     }
 
-    @FXML
-    void initialize() {
-
-    }
 }
--- CODE/MATClient/src/controller/RemoveStudentFromCourseController.java
+++ CODE/MATClient/src/controller/RemoveStudentFromCourseController.java
@@ -1,42 +1,72 @@
-package controller;
 
+package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class RemoveStudentFromCourseController {
+	
+	int sid, cid, oid;
+	
+	String isConfirmed, isHandeled;
 
     @FXML
     private ResourceBundle resources;
 
     @FXML
     private URL location;
+    
+    @FXML
+    private Button btnClose;
 
     @FXML
-    private TextField classNumberTextFiled;
+    private TextField studentIdTextField;
 
     @FXML
-    private TextField StudentIdTextField;
+    private TextField courseTextField;
 
     @FXML
-    private TextField courseIdTextField;
+    private TextField classTextField;
 
     @FXML
-    void SendRequest2(ActionEvent event) {
+    void sendRequest2(ActionEvent event) {
 
+    	if(studentIdTextField.getText().isEmpty() || courseTextField.getText().isEmpty()|| classTextField.getText().isEmpty()) {
+    		Alert alert = new Alert(AlertType.ERROR);
+    		alert.setHeaderText("one or more of the fields is empty");
+    		alert.show();
+    		Prompt.alert(3,"one or more of the fields is empty");
+    	}
+    	else { // add request to db
+    		try {
+    			sid = Integer.parseInt(studentIdTextField.getText());
+    			cid = Integer.parseInt(classTextField.getText());
+    	    	oid = Integer.parseInt(courseTextField.getText());
+    	    	isConfirmed = null;
+    	    	isHandeled = null;
+    	    	
+    	    	} catch(NumberFormatException e){
+    	    	Prompt.alert(3,"please enter numerical value");
+    	    	return;
+    	    	}  		
+        	Prompt.alert(2,"The request added successfully ");
+    	}
     }
-
+    
     @FXML
     void closeRequest2(ActionEvent event) {
+        Stage stage = (Stage) btnClose.getScene().getWindow();
+	    stage.close();
 
     }
 
-
-    @FXML
-    void initialize() {
-
-    }
 }
+
