89056de7eb47f07e80af79ac4aa5ca8ae4a4b397
TalChen <talush6691@gmail.com>
2017-06-24 22:31:08 +0300

    ,

---
+++ CODE/MATClient/src/controller/SubmissionStudentController.java
@@ -0,0 +1,41 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+
+public class SubmissionStudentController 
+{
+	@FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Label labelListOfStuSub;
+
+    @FXML
+    private Button buttonNext;
+
+    @FXML
+    private ComboBox<?> comboChooseSub;
+
+    @FXML
+    void initialize() 
+    {
+        
+    }
+}
+
+
+
+
+
+
+
+    
+
