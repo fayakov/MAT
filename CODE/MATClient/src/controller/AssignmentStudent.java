634d99e87d24a1b34b61577c570f906794c3ce95
TalChen <talush6691@gmail.com>
2017-06-24 17:43:56 +0300

    ,

---
+++ CODE/MATClient/src/controller/AssignmentStudent.java
@@ -0,0 +1,35 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+
+public class AssignmentStudent 
+{
+	@FXML
+    private Label labelMyAssignment;
+
+    @FXML
+    private ComboBox<String> comboChooseAssignment;
+
+    @FXML
+    private Button ButtonAssignmentNext;
+
+    @FXML
+    void AssignmentNext(ActionEvent event) 
+    {
+
+    }
+
+}
+
+
+
+
+
+
+    
+
+
--- CODE/MATClient/src/controller/StudentController.java
+++ CODE/MATClient/src/controller/StudentController.java
@@ -9,19 +9,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class StudentController 
 {
-	//int sID; 
-	//String sF, sL, sC;
 	ObservableList<String> list ;
-	String option;
 
     @FXML
     private ComboBox<String> optionCombo;
@@ -36,8 +30,18 @@ public class StudentController
 		Stage primaryStage = new Stage();
 		primaryStage.setScene(scene);
 		primaryStage.show();
-		
     }
+    
+    public void assignment() throws Exception 
+    {
+    	
+    	Pane root = FXMLLoader.load(getClass().getResource("/gui/AssignmentsStudent.fxml"));
+		Scene scene = new Scene(root);
+		Stage primaryStage = new Stage();
+		primaryStage.setScene(scene);
+		primaryStage.show();
+    }
+    
     
     public void start(Stage primaryStage) throws Exception 
     {
@@ -52,11 +56,11 @@ public class StudentController
     public void StudentInfo(ActionEvent event) throws Exception 
     {
     	
-	option = optionCombo.getValue().toString();
+	String option = optionCombo.getValue().toString();
     	if(option.equalsIgnoreCase("Student Data"))
     		studentData1();	
-    	//if(option.equalsIgnoreCase("Assignment"))
-    		//assignment();*
+    	if(option.equalsIgnoreCase("Assignment"))
+    		assignment();
     }
 
     @FXML
