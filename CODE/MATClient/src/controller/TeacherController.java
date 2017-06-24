a75384ad31882eb05f7c58ac4924a5adc3ac08f4
TalChen <talush6691@gmail.com>
2017-06-24 18:18:16 +0300

    t

---
+++ CODE/MATClient/src/controller/TeacherController.java
@@ -0,0 +1,100 @@
+package controller;
+
+import java.util.ArrayList;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class TeacherController 
+{
+	ObservableList<String> listT ;
+	
+	@FXML
+    private ComboBox<String> comboChooseOptionTeacher;
+
+    @FXML
+    private Label LabelTeacherMenu;
+
+    @FXML
+    private Button buttonSendTeacherMenu;
+
+    
+    void defineAssignment() throws Exception 
+    {
+        	
+        	Pane root = FXMLLoader.load(getClass().getResource("/gui/RequestRemoveStudentFromCourse.fxml"));
+    		Scene scene = new Scene(root);
+    		Stage primaryStage = new Stage();
+    		primaryStage.setScene(scene);
+    		primaryStage.show();
+        }
+
+    void checkAssignment() throws Exception 
+    {
+        	
+        	Pane root = FXMLLoader.load(getClass().getResource("/gui/RequestRemoveStudentFromCourse.fxml"));
+    		Scene scene = new Scene(root);
+    		Stage primaryStage = new Stage();
+    		primaryStage.setScene(scene);
+    		primaryStage.show();
+        }
+    
+    @FXML
+    void sendTeacherMenu(ActionEvent event) throws Exception 
+    {
+    	String option = comboChooseOptionTeacher.getValue().toString();
+    	if(option.equalsIgnoreCase("Define Assignment"))
+    		defineAssignment();	
+    	if(option.equalsIgnoreCase("Show Submission Of Student"))
+    		checkAssignment();
+    	
+    }
+    
+    
+
+    
+    
+    
+    public void start(Stage primaryStage) throws Exception 
+    {
+		
+		Parent root = FXMLLoader.load(getClass().getResource("/gui/TeachertMenu.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}   
+    
+    @FXML
+    void initialize() 
+    {
+        ArrayList<String> options = new ArrayList<String>();
+    	
+    	options.add("Define Assignment");
+      	options.add("Show Submission Of Student"); 
+      	
+		listT = FXCollections.observableArrayList(options);
+		comboChooseOptionTeacher.setItems(listT);
+       
+    }
+	
+}
+
+
+
+
+
+
+
+    
+
+
---
+++ CODE/MATClient/src/test/TeacherTest.java
@@ -0,0 +1,33 @@
+package test;
+
+import controller.DefineCourseController;
+import controller.TeacherController;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import controller.DefineCourseController;
+import controller.NewSemesterSettingController;
+
+import controller.secretaryController;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class TeacherTest extends Application 
+{
+
+	public static void main( String args[] ) throws Exception
+	   { 
+  launch(args);		
+	  }
+	
+	@Override
+	public void start(Stage arg0) throws Exception 
+	{
+		// TODO Auto-generated method stub
+		
+				TeacherController teaController = new TeacherController();
+				teaController.start(arg0);
+
+	}
+
+}
