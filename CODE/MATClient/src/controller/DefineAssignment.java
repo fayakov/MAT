e7f4c7a9b9d4e613343a6b7a0a1cd933f4e69884
TalChen <talush6691@gmail.com>
2017-06-24 21:31:51 +0300

    ,

---
+++ CODE/MATClient/src/controller/DefineAssignment.java
@@ -0,0 +1,58 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class DefineAssignment 
+{
+	@FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Label labelDefineAss;
+
+    @FXML
+    private Label labelUpload;
+
+    @FXML
+    private Label labelInsertClass;
+
+    @FXML
+    private Label labelInsertCourse;
+
+    @FXML
+    private DatePicker datePickerDefineDate;
+
+    @FXML
+    private TextField textFieldInsertClass;
+
+    @FXML
+    private TextField textFieldInsertCouse;
+
+    @FXML
+    private Label labelDefineDate;
+
+    
+    @FXML
+    void initialize() 
+    {
+       
+    }
+	
+}
+
+
+
+
+
+
+
+    
+
