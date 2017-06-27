10ee390186b0a287fce5c6541f557e8fca54120e
sapirSegall <sap456@walla.com>
2017-06-27 21:20:38 +0300

    dsd

--- CODE/MATCommon/src/communication/GetPendingRequestsRequest.java
+++ CODE/MATCommon/src/communication/GetPendingRequestsRequest.java
@@ -1,32 +1,30 @@
 package communication;
 
+import entities.ERequestType;
+
 public class GetPendingRequestsRequest extends Message {
 	private static final long serialVersionUID = 1L;
 	
-	private String userId;
-	private String password;
+
+	private ERequestType requestType;
+
 	
-	public GetPendingRequestsRequest(String userId, String password) {
-		this.userId = userId;
-		this.password = password;
-	}
 	
-	public String getUserId() {
-		return userId;
+
+	public GetPendingRequestsRequest(ERequestType requestType) {
+		super();
+		this.requestType = requestType;
 	}
 
-	public void setUserId(String userId) {
-		this.userId = userId;
+
+	public ERequestType getRequestType() {
+		return requestType;
 	}
 
-	public String getPassword() {
-		return password;
-	}
 
-	public void setPassword(String password) {
-		this.password = password;
+	public void setRequestType(ERequestType requestType) {
+		this.requestType = requestType;
 	}
-	
 	
 	
 }
