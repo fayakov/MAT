838f5ca88027ce82cb82fe538530224830824b61
sapirSegall <sap456@walla.com>
2017-06-27 21:00:31 +0300

    ff

--- CODE/MATClient/src/controller/ResponseAddStudentToCourseController.java
+++ CODE/MATClient/src/controller/ResponseAddStudentToCourseController.java
@@ -8,6 +8,8 @@ import communication.Message;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 import utils.Handler;
 import entities.ERequestType;
@@ -15,14 +17,35 @@ import javafx.fxml.FXML;
 
 public class ResponseAddStudentToCourseController {
 
-    @FXML
-    private ResourceBundle resources;
 
-    @FXML
-    private URL location;
 
-    @FXML
-    void initialize() {
+	    @FXML
+	    private ResourceBundle resources;
 
-    }
-}
+	    @FXML
+	    private URL location;
+
+	    @FXML
+	    private TableColumn<?, ?> studentId;
+
+	    @FXML
+	    private TableColumn<?, ?> requestNumber;
+
+	    @FXML
+	    private TableColumn<?, ?> isHandeled;
+
+	    @FXML
+	    private TableColumn<?, ?> classNumber;
+
+	    @FXML
+	    private TableColumn<?, ?> courseId;
+
+	    @FXML
+	    private TableView<?> table;
+
+	    @FXML
+	    void initialize() {
+	    }
+	}
+
+
