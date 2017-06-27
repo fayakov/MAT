7bce3b2c07a06e62b8c3b0c3a594b3fc3ce6e0ef
sapirSegall <sap456@walla.com>
2017-06-27 20:21:15 +0300

    d

---
+++ CODE/MATClient/src/controller/ResponseAddStudentToCourseController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import communication.Dispatcher;
+import communication.MATClientController;
+import communication.Message;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+import utils.Handler;
+import entities.ERequestType;
+import javafx.fxml.FXML;
+
+public class ResponseAddStudentToCourseController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+}
