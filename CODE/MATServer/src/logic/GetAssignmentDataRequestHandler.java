7bca343542ecfc225be9f83248ccb351d0696526
yinonsayada <yinonsayada@gmail.com>
2017-06-28 22:58:12 +0300

    Add stub handlers

---
+++ CODE/MATServer/src/logic/GetAssignmentDataRequestHandler.java
@@ -0,0 +1,34 @@
+package logic;
+
+import java.io.IOException;
+
+import DAL.CDALError;
+import DAL.CDal;
+import communication.LoginRequestMsg;
+import communication.LoginResponseMsg;
+import communication.Message;
+import ocsf.server.ConnectionToClient;
+import utils.Handler;
+
+public class GetAssignmentDataRequestHandler implements Handler {
+
+	public void handle(Message msg, Object obj) {
+		ConnectionToClient client = (ConnectionToClient) obj;
+		LoginRequestMsg loginMsg = (LoginRequestMsg)msg;
+		
+		System.out.println("User ID:\t" + loginMsg.getUserId());
+		System.out.println("User Password:\t" + loginMsg.getPassword());
+		
+		// TODO Check in database
+		CDALError error = new CDALError();
+		boolean connectionSecceded = CDal.connectUser(loginMsg.isToConnect(), loginMsg.getUserId(), loginMsg.getPassword(), error);		
+		
+		LoginResponseMsg res = new LoginResponseMsg(connectionSecceded, error.getString());
+		try {
+			client.sendToClient(res);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
