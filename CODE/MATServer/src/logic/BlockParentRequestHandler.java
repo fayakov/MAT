2486085f2c4bb7786444a77b3f1579d6b471081a
ymamane5 <ymamane5@gmail.com>
2017-07-01 14:54:19 +0300

    block parent

--- CODE/MATServer/src/logic/BlockParentRequestHandler.java
+++ CODE/MATServer/src/logic/BlockParentRequestHandler.java
@@ -1,6 +1,7 @@
 package logic;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import DAL.CDALError;
 import DAL.CDal;
@@ -17,9 +18,14 @@ public class BlockParentRequestHandler implements Handler {
 	public void handle(Message msg, Object obj) {
 		ConnectionToClient client = (ConnectionToClient) obj;
 		BlockParentRequest blockParentRequest = (BlockParentRequest)msg;
+		boolean requestSecceded = true;
+		ArrayList <Integer>childs = CDal.getParentsChildrens(blockParentRequest.getPid());
+		for(int i : childs)
+		{
+			requestSecceded &= CDal.blockParent(blockParentRequest.getPid(), i, blockParentRequest.toBlock());
+		}
 		
 		
-		boolean requestSecceded = CDal.blockParent(blockParentRequest.getPid(), 123, blockParentRequest.toBlock());
 		
 		BlockParentResponse res = new BlockParentResponse(requestSecceded, "");
 		
