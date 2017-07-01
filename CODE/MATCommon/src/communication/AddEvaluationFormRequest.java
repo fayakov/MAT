4238088cc2db95358a91b68dfb9692790f5c1caa
TalChen <talush6691@gmail.com>
2017-07-01 20:07:13 +0300

    ,

--- CODE/MATCommon/src/communication/AddEvaluationFormRequest.java
+++ CODE/MATCommon/src/communication/AddEvaluationFormRequest.java
@@ -1,34 +1,14 @@
 package communication;
 
+import entities.SubmissionResponse;
+
 public class AddEvaluationFormRequest extends Message 
 {
+	
 	private static final long serialVersionUID = 1L;
-	int grade;
-	String comment;
+	private SubmissionResponse s;
 	
 	
-	public AddEvaluationFormRequest(int grade, String comment) 
-	{
-		super();
-		this.grade = grade;
-		this.comment = comment;
-	}
-	
-	
-	public int getGrade() {
-		return grade;
-	}
-	public void setGrade(int grade) {
-		this.grade = grade;
-	}
-	public String getComment() {
-		return comment;
-	}
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
+
 
 }
