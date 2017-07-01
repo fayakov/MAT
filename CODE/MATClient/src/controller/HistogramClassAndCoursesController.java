28aa5e6c23161f153f4501254093e6b6195a3662
Tom Slilat <slilatom15@gmail.com>
2017-07-01 14:09:25 +0300

    //

--- CODE/MATClient/src/controller/HistogramClassAndCoursesController.java
+++ CODE/MATClient/src/controller/HistogramClassAndCoursesController.java
@@ -1,9 +1,14 @@
 package controller;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
-
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import communication.Message;
+import entities.TeacherWithGrade;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -13,27 +18,54 @@ import utils.Handler;
 
 public class HistogramClassAndCoursesController implements Initializable, Handler {
 	
+	private ArrayList<CourseWithGrade> arrToDisplay;
+
 	
+    public ArrayList<CourseWithGrade> getArrToDisplay() {
+		return arrToDisplay;
+	}
+
+	public void setArrToDisplay(ArrayList<CourseWithGrade> arrToDisplay) {
+		this.arrToDisplay = arrToDisplay;
+	}
+	
+	static int count = arrToDisplay.size();
+	private int i = 0;
+	
+	
+	@FXML
+    private CategoryAxis x;
 
     @FXML
-    private Button closebtn;
+    private NumberAxis y;
 
     @FXML
-    void Close(ActionEvent event) {
-	
-    	    Stage stage = (Stage) closebtn.getScene().getWindow();
-    	    stage.close();
-        }
+    private BarChart<?, ?> CourseChart;
+
+
 
 	public void handle(Message msg, Object obj) {
 		// TODO Auto-generated method stub
 		
-	}
+		XYChart<x,y>.Series set1 = new XYChart.Series<>();
+		for(int i=0; i<count;i++){
+		set1.getData().add(new XYChart.Data)();
+		}
+		CourseChart.getData().addAll(set1);
+		
+		
+		
+		}
 
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
 		
+		
+		
 	}
-        
-    }
+ 
+	
+	
+	
+}
 
--- CODE/MATCommon/src/communication/GetClassTeachersStatsResponse.java
+++ CODE/MATCommon/src/communication/GetClassTeachersStatsResponse.java
@@ -8,17 +8,35 @@ public class GetClassTeachersStatsResponse extends Message {
 	private static final long serialVersionUID = 1L;
 
 	private ArrayList<TeacherWithGrade> stats;
+	private String errText;
 
-	public GetClassTeachersStatsResponse(ArrayList<TeacherWithGrade> stats) {
+	
+	public GetClassTeachersStatsResponse(ArrayList<TeacherWithGrade> stats, String errText) {
 		super();
 		this.stats = stats;
+		this.errText = errText;
 	}
+
 
 	public ArrayList<TeacherWithGrade> getStats() {
 		return stats;
 	}
 
+
 	public void setStats(ArrayList<TeacherWithGrade> stats) {
 		this.stats = stats;
 	}
+
+
+	public String getErrText() {
+		return errText;
+	}
+
+
+	public void setErrText(String errText) {
+		this.errText = errText;
+	}
+
+	
+	
 }
