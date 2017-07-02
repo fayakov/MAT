9c08681632bd4b130163145aa7d5f8fd87bb1a68
ymamane5 <ymamane5@gmail.com>
2017-07-02 19:50:22 +0300

    junit

---
+++ CODE/MATClient/src/controller/PrerequisitesTest.java
@@ -0,0 +1,20 @@
+package controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class PrerequisitesTest extends TestCase {
+
+	
+	
+	
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
