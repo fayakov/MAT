b1dbd5ab63b3280a2b54ed797e0f5442e849bc9f
SagiYe <sagiyehe@gmail.com>
2017-07-03 21:37:55 +0300

    Hisograms

--- CODE/MATClient/src/controller/HistogramClassAndCoursesController.java
+++ CODE/MATClient/src/controller/HistogramClassAndCoursesController.java
@@ -7,60 +7,24 @@ import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
-import communication.Dispatcher;
-import communication.GetClassTeachersStatsResponse;
-import communication.Message;
 import entities.CourseWithGrade;
-import entities.TeacherWithGrade;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.stage.Stage;
-import utils.Handler;
 
 /**
  * The Class HistogramClassAndCoursesController.
  */
-public class HistogramClassAndCoursesController implements Initializable, Handler {
+public class HistogramClassAndCoursesController implements Initializable {
 	
 	/**
 	 * Instantiates a new histogram class and courses controller.
 	 */
 	public HistogramClassAndCoursesController(){
-		//Dispatcher.addHandler(GetClassCoursesStatsResponse.class.getCanonicalName(), this);
 	}
-	
-	
-	/** The arr to display. */
-	private ArrayList<CourseWithGrade> arrToDisplay;
-
-	
-    /**
-     * Gets the arr to display.
-     *
-     * @return the arr to display
-     */
-    public ArrayList<CourseWithGrade> getArrToDisplay() {
-		return arrToDisplay;
-	}
-
-	/**
-	 * Sets the arr to display.
-	 *
-	 * @param arrToDisplay the new arr to display
-	 */
-	public void setArrToDisplay(ArrayList<CourseWithGrade> arrToDisplay) {
-		this.arrToDisplay = arrToDisplay;
-	}
-	
-	/** The count. */
-	static int count = arrToDisplay.size();
 	
 	/** The i. */
 	private int i = 0;
-	
-	
+		
 	/** The x. */
 	@FXML
     private CategoryAxis x;
@@ -71,35 +35,13 @@ public class HistogramClassAndCoursesController implements Initializable, Handle
 
     /** The Course chart. */
     @FXML
-    private BarChart<?, ?> CourseChart;
-
-
-
-	/* (non-Javadoc)
-	 * @see utils.Handler#handle(communication.Message, java.lang.Object)
-	 */
-	public void handle(Message msg, Object obj) {
-		// TODO Auto-generated method stub
-		
-		//XYChart<x,y>.Series1 set1 = new XYChart.Series<>();
-		XYChart.Series<x, y> set1 = new XYChart.Series<>();
-		for(int i=0; i<count;i++){
-		set1.getData().add(new XYChart.Data)();
-		}
-		CourseChart.getData().addAll(set1);
-		
-		
-		
-		}
+    private BarChart<String, Number> CourseChart;
 
 	/* (non-Javadoc)
 	 * @see javafx.fxml.Initializable#initialize(java.net.URL, java.util.ResourceBundle)
 	 */
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
-		
-		
-		
 	}
 
 	/**
@@ -109,11 +51,13 @@ public class HistogramClassAndCoursesController implements Initializable, Handle
 	 */
 	public void initData(ArrayList<CourseWithGrade> arr) {
 		// TODO Auto-generated method stub
+		XYChart.Series set1 = new XYChart.Series<>();
+		
+		for (CourseWithGrade courseWithGrade : arr)
+			set1.getData().add(new XYChart.Data(courseWithGrade.getName(), courseWithGrade.getGrade()));
+		
+		CourseChart.getData().addAll(set1);
 		
 	}
- 
-	
-	
-	
 }
 
--- CODE/MATClient/src/controller/HistogramTeacherAndClassesController.java
+++ CODE/MATClient/src/controller/HistogramTeacherAndClassesController.java
@@ -8,6 +8,7 @@ import communication.Dispatcher;
 import communication.GetClassTeachersStatsResponse;
 import communication.Message;
 import entities.ClassWithGrade;
+import entities.TeacherWithGrade;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -24,17 +25,14 @@ import javafx.scene.chart.XYChart.Series;
 /**
  * The Class HistogramTeacherAndClassesController.
  */
-public class HistogramTeacherAndClassesController implements  Initializable, Handler{
+public class HistogramTeacherAndClassesController implements  Initializable {
 	
 	/**
 	 * Instantiates a new histogram teacher and classes controller.
 	 */
 	public HistogramTeacherAndClassesController(){
-	//	Dispatcher.addHandler(GetClassTeachersStatsResponse.class.getCanonicalName(), this);
 	}
-	
-	
-	
+		
 	 /** The Class chart. */
  	@FXML
 	 private BarChart<String, Number> ClassChart;
@@ -47,81 +45,28 @@ public class HistogramTeacherAndClassesController implements  Initializable, Han
     @FXML
     private NumberAxis y;
     
-	/** The arr to display. */
-	private ArrayList<ClassWithGrade> arrToDisplay;
-
-	
-    /**
-     * Gets the arr to display.
-     *
-     * @return the arr to display
-     */
-    public ArrayList<ClassWithGrade> getArrToDisplay() {
-		return arrToDisplay;
-	}
-
-	/**
-	 * Sets the arr to display.
-	 *
-	 * @param arrToDisplay the new arr to display
-	 */
-	public void setArrToDisplay(ArrayList<ClassWithGrade> arrToDisplay) {
-		this.arrToDisplay = arrToDisplay;
-	}
-	
-	//static int count = arrToDisplay.size();
-	//private int i = 0;
-	
-	
-	
-	
-	/* (non-Javadoc)
-	 * @see utils.Handler#handle(communication.Message, java.lang.Object)
-	 */
-	@Override
-	public void handle(Message msg, Object obj) {
-		// TODO Auto-generated method stub
-		
-		
-		
-		//XYChart<x,y>.Series set1 = new XYChart.Series<>();
-		//for(int i=0; i<count;i++){
-	//	set1.getData().add(new XYChart.Data)();
-		}
-		
-	//CourseChart.getData().addAll(set1);
-		
-
 	/* (non-Javadoc)
 	 * @see javafx.fxml.Initializable#initialize(java.net.URL, java.util.ResourceBundle)
 	 */
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * Inits the data.
+	 *
+	 * @param arr the arr
+	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public void initData(ArrayList<ClassWithGrade> arr) {
 		
-		 
+		XYChart.Series set1 = new XYChart.Series<>();
 		
-		ClassWithGrade c1 = new ClassWithGrade(7, "a1");
-		ClassWithGrade c2 = new ClassWithGrade(77, "a3");
-		ClassWithGrade c3 = new ClassWithGrade(26, "a4");
-		ClassWithGrade c4 = new ClassWithGrade(19, "a5");
-		
-		arrToDisplay.add(c1);
-		arrToDisplay.add(c2);
-		arrToDisplay.add(c3);
-		arrToDisplay.add(c4);
-		
-		XYChart.Series<String, Number> set1 = new XYChart.Series<>();
-		
-		
-		for (ClassWithGrade classWithGrade : arrToDisplay) {
+		for (ClassWithGrade classWithGrade : arr)
 			set1.getData().add(new XYChart.Data(classWithGrade.getName(), classWithGrade.getGrade()));
-		}
 		
-	//set1.getData().add(new XYChart.Data(arrToDisplay.get(58),arrToDisplay.getClass()));
-	//	set1.getData().add(new XYChart.Data(arrToDisplay.get(28),arrToDisplay.getClass()));
-		
-		
-		ClassChart.getData().add(set1);
+		ClassChart.getData().addAll(set1);
 	}
 }
